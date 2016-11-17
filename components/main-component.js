ebf7c4404ede1505df610ce7ea55d2f44cdd0775
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-11-17 09:47:30 -0700

    printer

--- components/main-component.js
+++ components/main-component.js
@@ -1,9 +1,36 @@
-; (function () {
-  angular.module('Resume')
-    .component('mainComponent', {
-      templateUrl: 'components/main-component.html',
 
-    })
 
+;(function () {
+    angular.module('DevResume')
+        .component('mainComponent', {
+            templateUrl: 'components/main-component.html',
+            controller: MainController,
+        })
+
+function MainController(){
+var mc= this;
+
+document.getElementById("btnPrint").onclick = function () {
+    printElement(document.getElementById("printThis"));
 }
-())
+
+function printElement(elem) {
+    var domClone = elem.cloneNode(true);
+    
+    var $printSection = document.getElementById("printSection");
+    
+    if (!$printSection) {
+        var $printSection = document.createElement("div");
+        $printSection.id = "printSection";
+        document.body.appendChild($printSection);
+    }
+    
+    $printSection.innerHTML = "";
+    $printSection.appendChild(domClone);
+    window.print();
+}
+
+
+
+    }
+} ());
